896cc44f4ddc8aa43ebd8d4d72027d6683d13538
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-07-05 17:33:40 +0530

    Week 1

---
+++ Week 1/Logging Framework/Exercise 1/Solution/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.error("This is an error message");
+        logger.warn("This is a warning message");
+
+    }
+}
