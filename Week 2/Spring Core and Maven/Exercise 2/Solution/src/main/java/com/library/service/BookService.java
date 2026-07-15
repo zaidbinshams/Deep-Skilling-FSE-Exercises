979314c7b98c51e9d58e3e1c55419edd38761af6
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-07-15 13:15:14 +0530

    Week 2 Spring Core and Maven Exercise 2

---
+++ Week 2/Spring Core and Maven/Exercise 2/Solution/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBook() {
+        System.out.println("Book : " + bookRepository.getBookTitle());
+    }
+
+}
