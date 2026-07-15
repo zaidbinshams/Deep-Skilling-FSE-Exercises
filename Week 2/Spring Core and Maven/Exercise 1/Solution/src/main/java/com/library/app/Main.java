a3542f2a15a8dfb98f9f2b1827d4f089cdcb50d1
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-07-15 13:14:27 +0530

    Week 2 Spring Core and Maven Exercise 1

---
+++ Week 2/Spring Core and Maven/Exercise 1/Solution/src/main/java/com/library/app/Main.java
@@ -0,0 +1,22 @@
+package com.library.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        service.displayBook();
+
+    }
+
+}
---
+++ Week 2/Spring Core and Maven/Exercise 1/Solution/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public String getBookTitle() {
+        return "Effective Java";
+    }
+
+}
