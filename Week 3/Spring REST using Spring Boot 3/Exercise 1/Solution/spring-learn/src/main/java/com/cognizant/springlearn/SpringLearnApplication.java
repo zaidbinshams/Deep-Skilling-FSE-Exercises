f075c8f5e0cf87da3e6ffcd19b2c7e5f1232b293
Zaid Bin Shams <zaidbinshams@gmail.com>
2026-07-17 10:08:21 +0530

    Week 3 Spring REST using Spring Boot 3 Exercise 1

---
+++ Week 3/Spring REST using Spring Boot 3/Exercise 1/Solution/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+}
